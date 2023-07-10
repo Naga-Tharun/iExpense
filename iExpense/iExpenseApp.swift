b4715d4a20c3295bd4f1ce3ab9eb93b789446c84
Naga-Tharun <nagatharunmakkena@gmail.com>
2023-07-10 12:55:11 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Naga Tharun Makkena on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
